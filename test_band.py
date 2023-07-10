3a380402b6ccca977703ba29f3b044f83858332b
dev-com2020 <kwp@tlen.pl>
2023-07-10 11:32:43 +0200

    11:32

--- test_band.py
+++ test_band.py
@@ -31,3 +31,28 @@ class BandLeader():
             print(f'Artist: {lines[1]}')
             if len(lines) > 2:
                 print(f'Genre: {lines[2]}')
+
+    def more_tracks(self, page='next'):
+        next_btn = [e for e in self.browser.find_elements(By.CLASS_NAME, 'item-page')
+                    if e.text.lower().strip() == str(page)]
+        if next_btn:
+            next_btn[0].click()
+            self.tracks()
+
+    def play(self, track=None):
+        if track is None:
+            self.browser.find_element(By.CLASS_NAME, 'playbutton').click()
+        elif type(track) is int and track <= len(self.track_list) and track >= 1:
+            self._current_track_number = track
+            self.track_list[self._current_track_number - 1].click()
+
+    def play_ext(self):
+        if self._current_track_number < len(self.track_list):
+            self.play(self._current_track_number + 1)
+        else:
+            self.more_tracks()
+            self.play(1)
+
+
+    def pause(self):
+        self.play()
